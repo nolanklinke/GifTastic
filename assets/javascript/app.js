8aefc60e8b0fd7d9f89b4429a6dabbed2d9d485b
disconaked <nolan.klinke@gmail.com>
2018-06-23 17:44:50 -0400

    basic js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,20 @@
 
 var topics = ["pizza", "nachos", "burgers", "fries", "waffles", "donuts", "pancakes", "ice cream", "vegetables", "fruit"]
 
-//For loop to create buttons within array
+//function with For loop to create buttons within array
+function createButtons () {
 
-for (var i = 0; i < topics.length; i++)  {
+    $("#buttons-go-here").empty();
 
-    var newBTN = $("<button>").text(topics[i]);
-    newBTN.addClass("btn btn-outline-primary m-2")
-    newBTN.attr("data-name", topics[i]);
-    $("#buttons-go-here").append(newBTN);
+
+    for (var i = 0; i < topics.length; i++)  {
+
+        var newBTN = $("<button>").text(topics[i]);
+        newBTN.addClass("btn btn-outline-primary m-2 searchInput")
+        newBTN.attr("data-name", topics[i]);
+        $("#buttons-go-here").append(newBTN);
     };
+};
 
 //create button option
 
@@ -19,12 +24,59 @@ $("#createBTN").on("click", function(event) {
     event.preventDefault();
     
     var userInput = $("#user-Input").val().trim();
-    var addBTN = $("<button>").text(userInput);
-    addBTN.addClass("btn btn-outline-primary m-2")
-    addBTN.attr("data-name", userInput);
-    $("#buttons-go-here").append(addBTN);
 
-    $("#user-Input").val("");
+    topics.push(userInput); 
+
+    createButtons();
 
 });
+
+// event listener for button elements to search giphy
+function displayInfo () {
+    
+    var search = $(this).attr("data-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=DI0K555bX6nANyYgvgEp7bz5h495nkiL&limit=25";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).then(function(response) {
+    
+    console.log(queryURL);
+    console.log(response);
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            
+            var gifSpan = $("<span>");
+
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height_still.url).addClass("m-2")
+
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            gifSpan.append(p);
+            gifSpan.append(gifImage);
+
+            $("#gifs-go-here").prepend(gifSpan);
+        }
+
+    }
+    
+    });
+
+
+};
+
+createButtons();
+$(document).on("click", ".searchInput", displayInfo);
+
+
+
+
         
